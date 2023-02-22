0b9d85c6258ea9c2a4afc06be66771fb61c99e6b
JulianaAmoasei <late.parrot@gmail.com>
2023-02-22 13:40:12 -0300

    test: :white_check_mark: add: testes em /eventos refatorados com unleash

--- src/test/routes/eventosRoutes.test.js
+++ src/test/routes/eventosRoutes.test.js
@@ -1,12 +1,14 @@
-/* eslint-disable no-unused-expressions */
 import { after } from 'mocha';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import sinon from 'sinon';
 import app from '../../app.js';
 import db from '../../db/dbconfig.js';
+import EventosController from '../../controllers/eventosController.js';
 
 chai.use(chaiHttp);
 const { expect } = chai;
+let stub;
 
 after(async () => {
   await db.destroy();
@@ -14,7 +16,7 @@ after(async () => {
 
 describe('GET em /eventos', () => {
   it('Deve retornar uma lista de eventos', (done) => {
-    process.env.EVENTO_FLAG = 'true';
+    stub = sinon.stub(EventosController, 'liberaAcessoEventos').returns(true);
     chai.request(app)
       .get('/eventos')
       .set('Accept', 'application/json')
@@ -28,7 +30,8 @@ describe('GET em /eventos', () => {
       });
   });
   it('Deve retornar erro 404', (done) => {
-    process.env.EVENTO_FLAG = 'false';
+    stub.restore();
+    stub = sinon.stub(EventosController, 'liberaAcessoEventos').returns(false);
     chai.request(app)
       .get('/eventos')
       .set('Accept', 'application/json')
